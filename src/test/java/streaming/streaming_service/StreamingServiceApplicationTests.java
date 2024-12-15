45bceb9ccceb6a9fa59bbf78c381a9c24e651b5a
capDoYeonLee <ghtyu21@naver.com>
2024-12-15 23:46:22 +0900

    first commit

---
+++ src/test/java/streaming/streaming_service/StreamingServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package streaming.streaming_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StreamingServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
