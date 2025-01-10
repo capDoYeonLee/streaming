f919a52566ddb1a1a61eccf68e5697f239ac27c0
capDoYeonLee <ghtyu21@naver.com>
2025-01-10 18:04:09 +0900

    feat: 프로젝트 config 추가

--- src/main/java/streaming/streaming_service/StreamingApplication.java
+++ src/main/java/streaming/streaming_service/StreamingApplication.java
@@ -2,12 +2,13 @@ package streaming.streaming_service;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 
-@SpringBootApplication
-public class StreamingServiceApplication {
+@SpringBootApplication(exclude={DataSourceAutoConfiguration.class})
+public class StreamingApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(StreamingServiceApplication.class, args);
+		SpringApplication.run(StreamingApplication.class, args);
 	}
 
 }
